264dfeea840305580499f9f59741d31b47935f28
ander <andersonfflores75@gmail.com>
2024-10-05 15:31:55 -0300

    enum of the types

---
+++ src/main/java/com/example/pokedex/Enums/Type.java
@@ -0,0 +1,23 @@
+package com.example.pokedex.Enums;
+
+public enum Type {
+
+    GRASS,
+    FIRE,
+    WATER,
+    BUG,
+    NORMAL,
+    POISON,
+    ELECTRIC,
+    GROUND,
+    FAIRY,
+    FIGHTING,
+    PSYCHIC,
+    ROCK,
+    GHOST,
+    ICE,
+    DRAGON,
+    DARK,
+    STEEL,
+    FLYING;
+}
